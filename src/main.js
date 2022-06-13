7d58078164f0986ec7730579bc30b97cc042239d
bianda880 <60204574+bianda880@users.noreply.github.com>
2022-06-13 15:46:51 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,29 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import { createWebHistory, createRouter } from "vue-router";
+
+import Home from "./components/Home.vue";
+import Kpop from "./components/Kpop.vue";
+import Kdrama from "./components/Kdrama.vue";
+import Kbeauty from "./components/Kbeauty.vue";
+import Entertaiment from "./components/Entertaiment.vue";
+import Lifestyle from "./components/Lifestyle.vue";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: "/", name: "Home", component: Home },
+    { path: "/kpop", name: "Kpop", component: Kpop },
+    { path: "/kdrama", name: "Kdrama", component: Kdrama },
+    { path: "/kbeauty", name: "Kbeauty", component: Kbeauty },
+    { path: "/entertaiment", name: "Entertaiment", component: Entertaiment },
+    { path: "/lifestyle", name: "Lifestyle", component: Lifestyle },
+  ],
+
+  base: "/",
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
